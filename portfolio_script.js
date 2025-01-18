138eb88826dbd5e415a2b920e77f116c392facd9
Gajam Manikumar <manikumargajam@gmail.com>
2025-01-18 18:05:16 +0530

    Initial commit

--- portfolio_script.js
+++ portfolio_script.js
@@ -1,10 +1,21 @@
 let menu =document.querySelector('#menu-icon');
 let navbar= document.querySelector('.navbar');
+const menuItems = document.querySelectorAll('.menu-item');
+
+
 
 menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('active');
+    
+    
+    navbar.style.textAlign='center';
+    
+    
 }
+
+
+
 
 window.onscroll =()=>{
     menu.classList.remove('bx-x');
@@ -12,7 +23,7 @@ window.onscroll =()=>{
 }
 
 const typed = new Typed('.multiple-text', {
-    strings: ['FullStack Developer','Frontend Developer', 'Backend Developer', 'Java Developer', 'web Designer',],
+    strings: ['FullStack Developer','Frontend Developer', 'Backend Developer', 'Java Developer',],
     typeSpeed: 80,
     backSpeed:80,
     backDelay:1200,
